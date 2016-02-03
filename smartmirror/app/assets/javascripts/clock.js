28d78a83716bb1ac2b1edf34f6fa7fc7c8fae507
Feras Deiratany <deiratany.1@osu.edu>
2016-02-02 22:49:10 -0500

    clock added

---
+++ smartmirror/app/assets/javascripts/clock.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+  var newDate = new Date();
+  newDate.setDate(newDate.getDate());
+  $('#Day').html(dayNames[newDate.getDay()] + "");
+  $('#Date').html(monthNames[newDate.getMonth()] + ' ' + newDate.getDate() + ', ' + newDate.getFullYear());
+
+  setInterval( function() {
+  // Create a newDate() object and extract the seconds of the current time on the visitor's
+  var seconds = new Date().getSeconds();
+  // Add a leading zero to seconds value
+  $("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+  },1000);
+  
+  setInterval( function() {
+    // Create a newDate() object and extract the minutes of the current time on the visitor's
+    var minutes = new Date().getMinutes();
+    // Add a leading zero to the minutes value
+    $("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
+      },1000);
+    
+  setInterval( function() {
+    // Create a newDate() object and extract the hours of the current time on the visitor's
+    var hours = new Date().getHours();
+    // Add a leading zero to the hours value
+    $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
+      }, 1000);
+
+});
